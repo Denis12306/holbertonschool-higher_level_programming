09995bfcd854a95008e80bd3357995436f857a7d
Denis12306 <denis.gavaud@gmail.com>
2026-01-15 13:30:30 +0100

    Modif

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
 
-def add(a, b):
-    return a + b
+    a = 1
+    b = 2
 
-
-a = 1
-b = 2
-resultat = add(a, b)
-print(f"{a} + {b} = {resultat}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
