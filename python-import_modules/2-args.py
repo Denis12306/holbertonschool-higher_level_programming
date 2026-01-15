2fea062799706905365ba173db40a5f14a194a94
Denis12306 <denis.gavaud@gmail.com>
2026-01-15 17:44:40 +0100

    ModifyFile

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,6 +4,7 @@ import sys
 if __name__ == "__main__":
     i = "Hello Welcome To The Best School"
     words = i.split()
+    print(f"{6} arguments:")
 
     for index, word in enumerate(words, start=1):
         print(f"{index}: {word}")
