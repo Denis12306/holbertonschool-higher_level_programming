f936f73eac15ecfef52df7784431bc414d004716
Denis12306 <denis.gavaud@gmail.com>
2026-01-15 17:48:07 +0100

    Task2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,14 @@ import sys
 if __name__ == "__main__":
     i = "Hello Welcome To The Best School"
     words = i.split()
-    print(f"{6} arguments:")
+
+    count = len(words)
+    if count == 0:
+        print("0 argument.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print(f"{count} arguments:")
 
     for index, word in enumerate(words, start=1):
         print(f"{index}: {word}")
