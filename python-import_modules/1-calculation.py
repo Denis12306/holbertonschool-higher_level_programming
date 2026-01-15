7bb68dc6deab3fd88a344f04cb86eeaaf0f29717
Denis12306 <denis.gavaud@gmail.com>
2026-01-15 14:03:44 +0100

    Modif

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
-def calculator_1(a, b):
-    print(f"{a} + {b}= {a+b}")
-    print(f"{a} - {b}= {a-b}")
-    print(f"{a} * {b}= {a*b}")
-    print(f"{a} / {b}= {a//b}")
-
-
 if __name__ == "__main__":
-    calculator_1(10, 5)
+    from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+print(f"{a} + {b} = {a+b}".format(a, b))
+print(f"{a} - {b} = {a-b}".format(a, b))
+print(f"{a} * {b} = {a*b}".format(a, b))
+print(f"{a} / {b} = {a//b}".format(a, b))
