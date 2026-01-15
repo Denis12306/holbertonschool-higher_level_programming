3b2472b7ba12ef8d273c43d0089f31be561db2af
Denis12306 <denis.gavaud@gmail.com>
2026-01-15 13:52:51 +0100

    Task1

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def calculator_1(a, b):
+    print(f"{a} + {b}= {a+b}")
+    print(f"{a} - {b}= {a-b}")
+    print(f"{a} * {b}= {a*b}")
+    print(f"{a} / {b}= {a//b}")
+
+
+if __name__ == "__main__":
+    calculator_1(10, 5)
