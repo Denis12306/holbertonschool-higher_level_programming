b284f4775d51699a645fe071a89855d73edee075
Denis12306 <denis.gavaud@gmail.com>
2026-01-15 17:30:24 +0100

    Task2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    i = "Hello Welcome To The Best School"
+    words = i.split()
+
+    for index, word in enumerate(words, start=1):
+        print(f"{index}: {word}")
