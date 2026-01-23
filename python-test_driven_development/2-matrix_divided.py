31134858729a75a0df9f2f2100482424bd521405
Denis12306 <denis.gavaud@gmail.com>
2026-01-23 15:16:37 +0100

    Task2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,37 +1,52 @@
 #!/usr/bin/python3
-=====================
-Module: 3-say_my_name
-=====================
+"""
+This module provides a function to divide all elements of a matrix by a number.
+"""
 
-Types
-=====
-``say_my_name()`` prints the string `My first name is
-<first name> <last name>`.  first_name and last_name
-must both be strings. last_name is optional.
 
-::
+def matrix_divided(matrix, div):
+    """
+    Divides all elements of a matrix by a number.
 
-	>>> say_my_name = __import__('3-say_my_name').say_my_name
-	>>> say_my_name("John", "Smith")
-	My name is John Smith
+    Args:
+        matrix (list of lists): A matrix (list of lists) of integers/floats.
+        div (int or float): The number to divide by.
 
-	>>> say_my_name("Bob")
-	My name is Bob
+    Returns:
+        list:A new matrix with elments divided and rounded to 2 decimal places.
 
-	>>> say_my_name(3, "Smith")
-	Traceback (most recent call last):
-	TypeError: first_name must be a string
+    Raises:
+        TypeError: If matrix is not a list of lists of integers/floats.
+        TypeError: If rows of matrix are not the same size.
+        TypeError: If div is not a number.
+        ZeroDivisionError: If div is zero.
+    """
+    if not type(div) in (int, float):
+        raise TypeError("div must be a number")
 
-	>>> say_my_name("John", 3)
-	Traceback (most recent call last):
-	TypeError: last_name must be a string
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
 
-	>>> say_my_name("")
-	My name is
+    msg_type = "matrix must be a matrix (list of lists) of integers/floats"
 
-	>>> say_my_name("[[This is a long string", "test]]")
-	My name is [[This is a long string test]]
+    if not matrix or not isinstance(matrix, list):
+        raise TypeError(msg_type)
 
-	>>> say_my_name()
-	Traceback (most recent call last):
-	TypeError: say_my_name() missing 1 required positional argument: 'first_name'
+    len_e = 0
+    msg_size = "Each row of the matrix must have the same size"
+
+    for elems in matrix:
+        if not elems or not isinstance(elems, list):
+            raise TypeError(msg_type)
+
+        if len_e != 0 and len(elems) != len_e:
+            raise TypeError(msg_size)
+
+        for num in elems:
+            if not type(num) in (int, float):
+                raise TypeError(msg_type)
+
+        len_e = len(elems)
+
+    m = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
+    return (m)
