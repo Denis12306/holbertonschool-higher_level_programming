336bb653eb9e0e9ad7b62b6f3332956ff0e00104
Denis12306 <denis.gavaud@gmail.com>
2026-01-23 14:12:40 +0100

    Task2

--- python-test_driven_development/tests/2-matrix_divided.py
+++ python-test_driven_development/tests/2-matrix_divided.py
@@ -1,58 +1,92 @@
 #!/usr/bin/python3
 """
-
-This module is composed by a function that divides the numbers of a matrix
-
+Module contenant une fonction divisant par 3 tous
+les nombres contenus dans une matrice
 """
 
 
 def matrix_divided(matrix, div):
-    """ Function that divides the integer/float numbers of a matrix
+    """
+    Fonction matrix_divided qui divise tous les nombres presents dans
+    une matrice.
 
-    Args:
-        matrix: list of a lists of integers/floats
-        div: number which divides the matrix
+    Parametres :
+        - matrix : matrice (tableau de tableau) qui contient seulement des
+        int ou des float
+        - div : int > 0 qui va diviser tous les nombres de matrix
 
-    Returns:
-        A new matrix with the result of the division
+    Return :
+        matrix divise par div, avec 2 chiffres decimaux max
 
-    Raises:
-        TypeError: If the elements of the matrix aren't lists
-                   If the elemetns of the lists aren't integers/floats
-                   If div is not an integer/float number
-                   If the lists of the matrix don't have the same size
-
-        ZeroDivisionError: If div is zero
-
-
+    Raise :
+        - TypeError si matrix ne contient pas que des int ou des float
+        - TypeError si les lignes de matrix ne contiennent pas le meme
+        nombre de colonnes
+        - TypeError si div n'est pas un nombre
+        - ZeroDivisionError si div == 0
     """
 
-    if not type(div) in (int, float):
-        raise TypeError("div must be a number")
+    # On verifie que la matrice contient au moins une valeur a diviser.
+    if matrix == []:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    # Puis on verifie que la matrice est une liste.
+    if not isinstance(matrix, list):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    # Si matrix est une liste simple qui ne contient que des int/float,
+    # alors il faut la transformer en liste de liste : [1, 2] -> [[1, 2]]
+    was_list = False
+    is_list = True
+    for elem in matrix:  # On parcourt les elements de matrix
+        if not isinstance(elem, (int, float)):
+            is_list = False
+            break
+            # Si elem n'est pas un chiffre, ce n'est pas une liste simple.
 
+    # Transformation liste simple en liste de liste
+    if is_list:
+        matrix = [matrix]
+        was_list = True
+
+    # On verifie ensuite que chaque element de matrix est une liste
+    for row in matrix:  # row est une liste de matrix
+        if not isinstance(row, list):
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats"
+                )
+        # On verifie enfin que la valeur dans la liste de liste
+        # est bien un nombre.
+        for val in row:  # val est la valeur contenue dans row
+            if not isinstance(val, (int, float)):
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of"
+                    " integers/floats")
+
+    list_size = len(matrix[0])  # Variable contenant la taille de la ligne 0
+    # Verification que toutes les lignes de matrix font la meme taille
+    for i in range(len(matrix)):
+        if len(matrix[i]) != list_size:
+            raise TypeError("Each row of the matrix must have the same size"
+                            )
+
+    # Verification div est un nombre
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    # Verification div est un nombre > 0
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    msg_type = "matrix must be a matrix (list of lists) of integers/floats"
+    mat2 = []  # Nouvelle matrice divisee
+    # Boucle ajoutant les valeurs modifiees dans la nouvelle matrixe.
+    for r in matrix:
+        line = []
+        for x in r:
+            line.append(round(x / div, 2))
+        mat2.append(line)
 
-    if not matrix or not isinstance(matrix, list):
-        raise TypeError(msg_type)
+    # Si la matrice initiale etait de dimension 1, on retourne une simple liste
+    if was_list:
+        mat2 = mat2[0]
 
-    len_e = 0
-    msg_size = "Each row of the matrix must have the same size"
-
-    for elems in matrix:
-        if not elems or not isinstance(elems, list):
-            raise TypeError(msg_type)
-
-        if len_e != 0 and len(elems) != len_e:
-            raise TypeError(msg_size)
-
-        for num in elems:
-            if not type(num) in (int, float):
-                raise TypeError(msg_type)
-
-        len_e = len(elems)
-
-    m = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
-    return (m)
+    return mat2
