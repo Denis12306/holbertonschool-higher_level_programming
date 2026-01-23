f82dd77b3d47f9a509983874adc49e4ed2e1083e
Denis12306 <denis.gavaud@gmail.com>
2026-01-23 15:13:04 +0100

    ModifFiles

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,92 +1,36 @@
-#!/usr/bin/python3
-"""
-Module contenant une fonction divisant par 3 tous
-les nombres contenus dans une matrice
-"""
+=====================
+Module: 3-say_my_name
+=====================
 
+Types
+=====
+``say_my_name()`` prints the string `My first name is
+<first name> <last name>`.  first_name and last_name
+must both be strings. last_name is optional.
 
-def matrix_divided(matrix, div):
-    """
-    Fonction matrix_divided qui divise tous les nombres presents dans
-    une matrice.
+::
 
-    Parametres :
-        - matrix : matrice (tableau de tableau) qui contient seulement des
-        int ou des float
-        - div : int > 0 qui va diviser tous les nombres de matrix
+	>>> say_my_name = __import__('3-say_my_name').say_my_name
+	>>> say_my_name("John", "Smith")
+	My name is John Smith
 
-    Return :
-        matrix divise par div, avec 2 chiffres decimaux max
+	>>> say_my_name("Bob")
+	My name is Bob
 
-    Raise :
-        - TypeError si matrix ne contient pas que des int ou des float
-        - TypeError si les lignes de matrix ne contiennent pas le meme
-        nombre de colonnes
-        - TypeError si div n'est pas un nombre
-        - ZeroDivisionError si div == 0
-    """
+	>>> say_my_name(3, "Smith")
+	Traceback (most recent call last):
+	TypeError: first_name must be a string
 
-    # On verifie que la matrice contient au moins une valeur a diviser.
-    if matrix == []:
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats")
-    # Puis on verifie que la matrice est une liste.
-    if not isinstance(matrix, list):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats")
-    # Si matrix est une liste simple qui ne contient que des int/float,
-    # alors il faut la transformer en liste de liste : [1, 2] -> [[1, 2]]
-    was_list = False
-    is_list = True
-    for elem in matrix:  # On parcourt les elements de matrix
-        if not isinstance(elem, (int, float)):
-            is_list = False
-            break
-            # Si elem n'est pas un chiffre, ce n'est pas une liste simple.
+	>>> say_my_name("John", 3)
+	Traceback (most recent call last):
+	TypeError: last_name must be a string
 
-    # Transformation liste simple en liste de liste
-    if is_list:
-        matrix = [matrix]
-        was_list = True
+	>>> say_my_name("")
+	My name is
 
-    # On verifie ensuite que chaque element de matrix est une liste
-    for row in matrix:  # row est une liste de matrix
-        if not isinstance(row, list):
-            raise TypeError(
-                "matrix must be a matrix (list of lists) of integers/floats"
-                )
-        # On verifie enfin que la valeur dans la liste de liste
-        # est bien un nombre.
-        for val in row:  # val est la valeur contenue dans row
-            if not isinstance(val, (int, float)):
-                raise TypeError(
-                    "matrix must be a matrix (list of lists) of"
-                    " integers/floats")
+	>>> say_my_name("[[This is a long string", "test]]")
+	My name is [[This is a long string test]]
 
-    list_size = len(matrix[0])  # Variable contenant la taille de la ligne 0
-    # Verification que toutes les lignes de matrix font la meme taille
-    for i in range(len(matrix)):
-        if len(matrix[i]) != list_size:
-            raise TypeError("Each row of the matrix must have the same size"
-                            )
-
-    # Verification div est un nombre
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
-    # Verification div est un nombre > 0
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-
-    mat2 = []  # Nouvelle matrice divisee
-    # Boucle ajoutant les valeurs modifiees dans la nouvelle matrixe.
-    for r in matrix:
-        line = []
-        for x in r:
-            line.append(round(x / div, 2))
-        mat2.append(line)
-
-    # Si la matrice initiale etait de dimension 1, on retourne une simple liste
-    if was_list:
-        mat2 = mat2[0]
-
-    return mat2
+	>>> say_my_name()
+	Traceback (most recent call last):
+	TypeError: say_my_name() missing 1 required positional argument: 'first_name'
--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,30 +1,12 @@
 #!/usr/bin/python3
-"""
-Module contenant une fonction affichant deux chaines de caractere.
-"""
+""" 3-say_my_name Module """
 
 
 def say_my_name(first_name, last_name=""):
-    """
-    Fonction say_my_name affiche les chaines de caractere first_name
-    et last_name.
-
-    Parametres :
-        - first_name : string
-        - last_name : string
-
-    Return :
-        Chaine de caractere : "My name is <first_name> <last_name>"
-
-    Raise :
-        - TypeError si first_name n'est pas un string
-        - TypeError si last_name n'est pas un string
-    """
-
-    if not isinstance(first_name, str):
+    """ Function that prints the input names """
+    if type(first_name) != str:
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if type(last_name) != str:
         raise TypeError("last_name must be a string")
 
-    phrase = "My name is {} {}".format(first_name, last_name)
-    print(phrase)
+    print("My name is", first_name, last_name)
