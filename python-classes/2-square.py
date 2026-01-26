ffe10143debe213b030be1a31f74e98191334850
Denis12306 <denis.gavaud@gmail.com>
2026-01-26 15:11:41 +0100

    Task2

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Write a class Square that defines a square by: (based on 1-square.py)'''
+
+
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            '''Size must be an integer otherwaise raise a TypeError'''
+            '''If size is less than 0 raise a ValueError'''
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        print(size)
