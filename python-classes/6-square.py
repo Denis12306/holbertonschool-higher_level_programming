34d9e9371d6898c19fda23ad250bed333491f91b
Denis12306 <denis.gavaud@gmail.com>
2026-01-27 14:14:24 +0100

    Task6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -3,8 +3,9 @@
 
 
 class Square:
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         self.__size = size
+        self.__position = position
 
     @property
     def size(self):
@@ -19,8 +20,22 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-        
+
+    @property
     def position(self):
+        """Getter: return the position of the square"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Check if position is a tuple of two integers"""
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int or value[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[1]) is not int or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """Return the area of the square."""
@@ -31,5 +46,11 @@ class Square:
         if self.size == 0:
             print("")
         else:
-            for _ in range(self.size):
-                print("#" * self.size)
+            for i in range(self.position[1]):
+                print()
+            for i in range(self.size):
+                for j in range(self.position[0]):
+                    print(end=" ")
+                for k in range(self.size):
+                    print("#", end="")
+                print()
