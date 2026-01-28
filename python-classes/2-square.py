5182e6f164cf6bae31a7fa571d10d4008409ae1e
Denis12306 <denis.gavaud@gmail.com>
2026-01-28 14:24:16 +0100

    ModifFile

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""Write a class Square that defines a square by: (based on 1-square.py)"""
+"""A class Square that defines a square by: (based on 1-square.py)"""
 
 
 class Square:
     """Size must be an integer otherwaise raise a TypeError"""
     def __init__(self, size=0):
         """If size is less than 0 raise a ValueError"""
-        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        print(size)
+        self.__size = size
