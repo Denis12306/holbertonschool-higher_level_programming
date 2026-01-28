d286ee18e5da9e80b64b9763e6c710c226b8c136
Denis12306 <denis.gavaud@gmail.com>
2026-01-28 14:25:47 +0100

    FormatDoc

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Write a class Square that defines a square by: (based on 2-square.py)"""
+"""A class Square that defines a square by: (based on 2-square.py)"""
+
 
 class Square:
     """Size must be an integer otherwaise raise a TypeError"""
-    """If size is less than 0 raise a ValueError"""
+
     def __init__(self, size=0):
+        """If size is less than 0 raise a ValueError"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = size
 
     def area(self):
