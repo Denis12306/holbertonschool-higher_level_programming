25ec62838476c4de6db8822ffb6994fe9e366f41
Denis12306 <denis.gavaud@gmail.com>
2026-02-03 20:10:20 +0100

    Task5

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+"""Design two mixin classes, SwimMixin and FlyMixin, each equipped with methods
+swim and fly respectively. Next, construct a class Dragon that inherits from
+both these mixins. Your aim is to show that a Dragon instance can both
+swim and fly."""
+
+
+class SwimMixin:
+    """Define the Mixin Swim class"""
+
+    def swim(self):
+        print("The creature swims!")
+
+
+class FlyMixin:
+    """Define the Mixin Fly class"""
+
+    def fly(self):
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    """Define the Dragon class with 2 previous class"""
+
+    def roar(self):
+        print("The Dragon roars!")
+
+    def spit_fireball(self):
+        print("The Dragon spit a fireball!")
+
+        draco = Dragon()
+        draco.swim()
+        draco.fly()
+        draco.roar()
+        draco.spit_fireball()
