34569dc89292e6c0351877857efe38ff802d920c
Denis12306 <denis.gavaud@gmail.com>
2026-02-04 11:28:34 +0100

    Comment

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Define a class MyList that inherits from list"""
+"""Define a class MyList that inherits to list"""
 
 
 class MyList(list):
-    """Represent a class MyList that inherits from list"""
+    """Represent a class MyList that inherits to list"""
 
     def print_sorted(self):
         """Print the list sorted in ascending order"""
