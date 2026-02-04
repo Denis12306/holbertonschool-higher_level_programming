b7ef4d0871d6f806ce665c0991e4e20c627ecefe
Denis12306 <denis.gavaud@gmail.com>
2026-02-04 12:03:42 +0100

    Comments

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -19,15 +19,19 @@ class Circle(Shaped):
     """Represent a class Circle inherited from Shaped"""
 
     def __init__(self, radius):
+         """Initialize the circle with a radius."""
         self.radius = radius
 
     def area(self):
+        """Calculate the area of the circle"""
         return math.pi * self.radius * self.radius
 
     def perimeter(self):
+        """Calculate the perimeter of the circle"""
         return 2 * math.pi * self.radius
 
     def shape_info(self):
+        """Print the area and perimeter of the shape."""
         return f"[Circle] radius={self.radius}"
 
 
@@ -35,20 +39,24 @@ class Rectangle(Shaped):
     """Represent a class Rectangle inherited from Shaped"""
 
     def __init__(self, width, height):
+        """Initialize the rectangle with width and height."""
         self.width = width
         self.height = height
 
     def area(self):
+        """Calculate the area of the rectangle."""
         return self.width * self.height
 
     def perimeter(self):
+        """Calculate the perimeter of the rectangle."""
         return 2 * (self.width + self.height)
 
     def shape_info(self):
+        """Print the area and perimeter of the shape."""
         return f"[Rectangle] {self.width}/{self.height}"
 
 
 def shape_info(obj):
-    """Duck typing info"""
+    """Print the area and perimeter of the shape."""
     print("Area:", obj.area())
     print("Perimeter:", obj.perimeter())
