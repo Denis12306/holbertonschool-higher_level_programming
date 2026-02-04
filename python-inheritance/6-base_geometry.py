9b5615e7c599486f579afc32c1e718130b1d3a8c
Denis12306 <denis.gavaud@gmail.com>
2026-02-04 10:55:30 +0100

    Comment

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Define an empty class BaseGeometry"""
+
+
 class BaseGeometry:
     """Represent an empty class BaseGeometry"""
+
     def area(self):
         raise Exception("area() is not implemented")
