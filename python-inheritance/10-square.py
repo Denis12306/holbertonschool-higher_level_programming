7975d132e809bc128dcffdf2deaf38564ce119fb
Denis12306 <denis.gavaud@gmail.com>
2026-02-04 12:14:07 +0100

    ModifFile

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -35,8 +35,14 @@ class Rectangle(BaseGeometry):
 
 
 class Square(Rectangle):
-    """Represent a class Square with Rectangle attributs """
+    """Represent a square."""
 
     def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
