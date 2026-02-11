e723324cfaa83bb785ffaeea333edfe6ecb8e9e6
Denis12306 <denis.gavaud@gmail.com>
2026-02-11 08:13:53 +0100

    AddEnd

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Represent the file we want to read"""
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
