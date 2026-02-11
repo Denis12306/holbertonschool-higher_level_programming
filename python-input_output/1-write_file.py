28c80ed9ced1bf61417e66ddf833f6fbc78a3f50
Denis12306 <denis.gavaud@gmail.com>
2026-02-11 08:15:15 +0100

    Comment

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,6 +4,7 @@ the number of characters written:"""
 
 
 def write_file(filename="", text=""):
+    """Represent the file to write in"""
     with open(filename, "w+", encoding="utf-8") as f:
         f.write(text)
         return len(text)
