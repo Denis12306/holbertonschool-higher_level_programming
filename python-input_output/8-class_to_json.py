e04c5a90343e74b8d04936fe6f6e09e27128c596
Denis12306 <denis.gavaud@gmail.com>
2026-02-11 09:06:19 +0100

    Task8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,7 +2,9 @@
 """A function that returns the dictionary description with simple data
 structure (list, dictionary, string, integer and boolean)
 for JSON serialization of an object"""
+import json
 
 
 def class_to_json(obj):
-
+    """Represent the dict to return"""
+    return obj.__dict__
