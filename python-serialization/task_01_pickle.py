4129427f8d29b1ffbd33b16a47f16ca7fdd6c99a
Denis12306 <denis.gavaud@gmail.com>
2026-02-11 15:55:19 +0100

    Comment

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -6,16 +6,20 @@ import pickle
 
 class CustomObject:
     """Represent a class CustomObjet"""
+
     def __init__(self, name, age, is_student):
+        """Initialised the 3 asking parameters"""
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def serialize(self, filename):
+        """This method will take a filename as its parameter"""
         with open(filename, "wb") as f:
             pickle.dump(self, f)
 
     @classmethod
     def deserialize(cls, filename):
+        """Deserialised the object"""
         with open(filename, "rb") as f:
             return pickle.load(f)
