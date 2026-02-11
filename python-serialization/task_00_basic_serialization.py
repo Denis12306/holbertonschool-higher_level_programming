87d8d9b7a59c7f5b5bb1f913f2065f930c06946b
Denis12306 <denis.gavaud@gmail.com>
2026-02-11 15:58:00 +0100

    Task0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -2,16 +2,20 @@
 """Create a basic serialization module that adds the functionality to
 serialize a Python dictionary to a JSON file and deserialize the JSON file
 to recreate the Python Dictionary."""
-import pickle
+import json
 
 
 def serialize_and_save_to_file(data, filename):
-    """Represent the Python dictionary added"""
-    with open(filename, "wb") as f:
-        pickle.dump(data, f)
+    try:
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(data, f)
+    except Exception:
+        return None
 
 
 def load_and_deserialize(filename):
-    """Represent the deserialized JSON data from the file"""
-    with open(filename, "rb") as f:
-        return pickle.load(f)
+    try:
+        with open(filename, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except Exception:
+        return None
