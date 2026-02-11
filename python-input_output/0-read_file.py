49eb3bb09cb0d8d13afe0b2a694dd33fc3c06a44
Denis12306 <denis.gavaud@gmail.com>
2026-02-11 08:10:12 +0100

    ModifFile

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A function that reads a text file (UTF8) and prints it to stdout:"""
+"""A function that reads a text file (UTF8) and prints it to stdout"""
 
 
 def read_file(filename=""):
--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,5 +2,6 @@
 """A function that returns the JSON representation of an object (string)"""
 import json
 
+
 def to_json_string(my_obj):
     return json.dumps(my_obj)
