f238ddd2e0af420f7b2b25fed6ce98da7b6a7492
Denis12306 <denis.gavaud@gmail.com>
2026-03-06 12:50:36 +0100

    Task10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Write a script that prints the State object with the name passed
+as argument from the database hbtn_0e_6_usa
+Your script should take 4 arguments: mysql username, mysql password,
+database name and state name to search (SQL injection free)
+You must use the module SQLAlchemy
+You must import State and Base from model_state -
+from model_state import Base, State
+Your script should connect to a MySQL server running on localhost at port 3306
+You can assume you have one record with the state name to search
+Results must display the states.id
+If no state has the name you searched for, display Not found
+Your code should not be executed when imported"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format
+        (user, password, database),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == state_name).first()
+
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
+    session.close()
