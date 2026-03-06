808547184cc5d4f06dbc40d30445ff24b3658634
Denis12306 <denis.gavaud@gmail.com>
2026-03-06 10:28:51 +0100

    task2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     )
     cursor = db.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+        "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(
             sys.argv[4]
         )
     )
