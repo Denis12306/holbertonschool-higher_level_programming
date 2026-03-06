f1abad2ff1be1dcae23c6e28750b74f04c8fc88c
Denis12306 <denis.gavaud@gmail.com>
2026-03-06 14:51:12 +0100

    ModelCity

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -2,14 +2,17 @@
 """
 Module qui définit la classe City
 """
-
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
+
 class City(Base):
-    """Classe City qui mappe la table 'cities'"""
+    """Classe City qui représente la table 'cities'"""
+
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False)
+
     name = Column(String(128), nullable=False)
+
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
