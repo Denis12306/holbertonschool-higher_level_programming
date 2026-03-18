4e5b3f0283dd4e1229841167fb0bc787c9375c07
Denis12306 <denis.gavaud@gmail.com>
2026-03-18 15:40:20 +0100

    Task2-3

---
+++ javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* Write a script that prints the first argument passed to it:
+   If no arguments are passed to the script, print "No argument"
+   You must use console.log(...) to print all output
+   You are not allowed to use var
+   You are not allowed to use length
+*/
+const firstArg = process.argv[2];
+if (firstArg === undefined) {
+  console.log('No argument');
+} else {
+  console.log(firstArg);
+}
