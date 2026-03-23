c610aef43c53774d89d23720e0feaf309d887ebb
Denis12306 <denis.gavaud@gmail.com>
2026-03-23 15:12:41 +0100

    Task1 and 2

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/*  Write a JavaScript script that updates the text color of the header element to red (#FF0000):
+    You must use document.querySelector to select the HTML tag
+*/
+const header = document.querySelector('header');
+header.style.color = '#FF0000';
---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* Write a JavaScript script that adds the class red to the header element
+   when the user clicks on the tag with id red_header
+*/
+const trigger = document.querySelector("#red_header");
+
+const header = document.querySelector("header");
+
+trigger.addEventListener("click", function () {
+  header.classList.add("red");
+});
