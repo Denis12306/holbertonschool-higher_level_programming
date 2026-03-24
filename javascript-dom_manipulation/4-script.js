164bb202b5faa1871c0cf9add24d6e78a03ca68b
Denis12306 <denis.gavaud@gmail.com>
2026-03-24 09:56:22 +0100

    Task4

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/* Write a JavaScript script that adds a li element to a list when the user
+   clicks on the element with id add_item:
+   The new element must be: <li>Item</li> The new element must be added to the
+   ul element with class my_list
+*/
+const addButton = document.querySelector("#add_item");
+
+const list = document.querySelector(".my_list");
+
+addButton.addEventListener("click", function() {
+  const newItem = document.createElement("li");
+
+  newItem.textContent = "Item";
+
+  list.appendChild(newItem);
+});
